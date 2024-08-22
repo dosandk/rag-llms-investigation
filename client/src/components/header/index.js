5a3da2197d16222e3790a6f68faedea99a781b80
Volodymyr Shevchuk <dosandkv@gmail.com>
2024-08-22 13:38:07 +0300

    clear unused env values, add link to github repo

--- client/src/components/header/index.js
+++ client/src/components/header/index.js
@@ -10,6 +10,18 @@ export default class Header extends BaseComponent {
   }
 
   get template() {
-    return <div class="app-header"></div>;
+    return (
+      <div class="app-header">
+        <div class="container-fluid h-100 d-flex align-items-center justify-content-end">
+          <a
+            class="dropdown-item w-auto"
+            target="_blank"
+            href="https://github.com/dosandk/rag-llms-investigation"
+          >
+            Source code: <i class="bi bi-github"></i>
+          </a>
+        </div>
+      </div>
+    );
   }
 }
